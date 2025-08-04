7b757dfe33242a885dc5917bdd14eaa6ae927b3d
Mahmoud Atif <mahmoudateff07@gmail.com>
2025-08-05 02:09:56 +0300

    format

--- user/decorators.py
+++ user/decorators.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 def authenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('home')
+            return redirect("home")
         else:
             return view_func(request, *args, **kwargs)
 
